aa8e61ac0fead64e7e2b30b095e716d6b808f712
kishanrajasekhar <krajasek@uci.edu>
2017-03-31 22:43:10 -0700

    completed game logic, just need to update view

--- othello_script.js
+++ othello_script.js
@@ -34,13 +34,90 @@ function othello_logic(rows, cols, first_turn, position){
 		}
 		console.log('---------');
 	};
+	
+	/* Returns a list of locations where the pieces 
+	can be flipped */
+	this.locations_to_flip = function(row, col){
+		if(this._board[row][col] == '.'){
+			var result = []
+			// a list of functions
+			var directions = [north, south, east, west,
+			north_east, north_west, south_east,
+			south_west];
+			for(var i=0; i<directions.length; i++){
+				var dir = directions[i](this._board, row, col);
+				if(this.can_squish(dir, this.get_turn_color())){
+					result.push(dir);
+				}
+			}
+			return result;
+		}else{
+			return [];
+		}
+	};
+	
+	/* Returns true if the othello piece flip the other
+	othello pieces in the list */
+	this.can_squish = function(list, othello_piece){
+		if(list.length >=2){
+			var piece = this._board[list[0][0]][list[0][1]];
+			if(piece == '.' || piece == othello_piece){
+				return false;
+			}
+			for(var i=1; i<=list.length; i++){
+				piece = this._board[list[i][0]][list[i][1]];
+				if(piece == othello_piece){
+					return true;
+				}
+				if(piece == '.'){
+					return false;
+				}
+			}
+			return false;
+		}else{
+			return false;
+		}
+	};
+	
+	/*Flips the pieces in locations_list to match the same color
+        as the piece placed in the specified row and column.
+		locations_list is a list of list of locaitons (row, col)*/
+	this.flip = function(locations_list){
+		for(var i=0; i<locations_list.length; i++){
+			for(var j=0; j<locations_list[i].length; j++){
+				var row = locations_list[i][j][0];
+				var col = locations_list[i][j][1];
+				console.log(this._board[row][col]);
+				if(this._board[row][col] == this.get_opposite_color()){
+					this._board[row][col] = this.get_turn_color();
+				}else{
+					break;
+				}
+			}
+		}
+	};
+	
 	//return which player's turn it is
 	this.get_turn = function(){
 		return this._turn;
 	};
+	this.get_turn_color = function(){
+		if(this._turn == "white"){
+			return 'w';
+		}else{
+			return 'b';
+		}
+	};
+	this.get_opposite_color = function(){
+		if(this._turn == "white"){
+			return 'b';
+		}else{
+			return 'w';
+		}
+	};
     this.get_board = function(){
         return this._board;
-    }
+    };
 }
 
 //changes who's turn it is
@@ -103,18 +180,18 @@ function start_game(rows, cols){
       var row = parseInt(board_locs[0]);
       var col = parseInt(board_locs[1]);
 	  if(o.get_piece(row,col) == '.'){
-          var temp = direction(o.get_board(),row,col,1,1);
-          console.log(temp);
-          // TODO: before adding, check if it's a valid move
-		  $(this).html('<img alt="" height="100%" width="100%" src="' + o.get_turn() + '_piece.PNG">');
-		  o.add_piece(board_locs[0], board_locs[1]);
-		  $("#turn").text("Player with " + o.get_turn() + " pieces, make a move.");
-		  o.log_board(); 
+		  var locs_to_flip = o.locations_to_flip(row,col);
+		  if(locs_to_flip.length > 0){
+			  o.flip(locs_to_flip);
+			  // TODO: before adding, check if it's a valid move
+			  $(this).html('<img alt="" height="100%" width="100%" src="' + o.get_turn() + '_piece.PNG">');
+			  o.add_piece(board_locs[0], board_locs[1]);
+			  $("#turn").text("Player with " + o.get_turn() + " pieces, make a move.");
+			  o.log_board(); 
+		  }
 	  }
 	});
 }
-
-// functions for checking adjacent locations
 
 /*Returns a list of locations (row and col tuples) of the spaces in the
  board of the direction of row_increment, col_increment. This function will
@@ -131,6 +208,55 @@ function direction(board, row, col, row_increment, col_increment){
     }
     return result;
 }
+
+/*Returns all the locations left (or west of) the specified location
+    and returns them in a list.*/
+function west(board, row, column){
+    return direction(board, row, column, 0, -1);
+}
+
+/*Returns all the locations right (or east of) the specified location
+    and returns them in a list.*/
+function east(board, row, column){
+    return direction(board, row, column, 0, 1);
+}
+
+/*Returns all the locations above (or north of) the specified location
+    and returns them in a list.*/
+function north(board, row, column){
+    return direction(board, row, column, -1, 0);
+}
+
+/*Returns all the locations below (or south of) the specified location
+    and returns them in a list.*/
+function south(board, row, column){
+    return direction(board, row, column, 1, 0);
+}
+
+/*Returns all the locations upper left diagonal (north west) the specified location
+    and returns them in a list.*/
+function north_west(board, row, column){
+    return direction(board, row, column, -1, -1);
+}
+
+/*Returns all the locations upper right diagonal (north east) the specified location
+    and returns them in a list.*/
+function north_east(board, row, column){
+    return direction(board, row, column, -1, 1);
+}
+
+/*Returns all the locations lower left diagonal (south west) the specified location
+    and returns them in a list.*/
+function south_west(board, row, column){
+    return direction(board, row, column, 1, -1);
+}
+
+/*Returns all the locations lower right diagonal (south east) the specified location
+    and returns them in a list.*/
+function south_east(board, row, column){
+    return direction(board, row, column, 1, 1);
+}
+
 
 //onload function important! HTML need to load first	
 window.onload = function(){	
